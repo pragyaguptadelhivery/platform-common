5be13d0dbd87f442ad4a63a8f9d1087d8f59c875
pragyaguptadelhivery <pragya.gupta@delhivery.com>
2020-04-23 09:40:29 +0530

    ELstic wrapper code

---
+++ src/main/java/client/Applicatiobn.java
@@ -0,0 +1,14 @@
+package client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applicatiobn {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Applicatiobn.class, args);
+        }
+
+}
---
+++ src/main/java/client/repository/EmployeeRepository.java
@@ -0,0 +1,4 @@
+package client.repository;
+
+public class EmployeeRepository {
+}
